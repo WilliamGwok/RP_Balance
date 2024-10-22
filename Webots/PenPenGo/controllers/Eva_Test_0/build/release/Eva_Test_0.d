38cb0379c17661435722b61b0d918044ad41176f
Will <junweigao1@outlook.com>
2024-10-22 22:33:07 +0800

    delete penpengo

--- Webots/PenPenGo/controllers/Eva_Test_0/build/release/Eva_Test_0.d
+++
@@ -1,8 +0,0 @@
-build/release/Eva_Test_0.o: Eva_Test_0.c \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h
--- Webots/PenPenGo/controllers/Eva_Test_1/build/release/Eva_Test_1.d
+++
@@ -1,9 +0,0 @@
-build/release/Eva_Test_1.o: Eva_Test_1.c \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
- C:\Users\Lenovo\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h
